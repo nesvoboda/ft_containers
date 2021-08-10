c661b74c1e30c9da0b9647723935f75a94bace79
Arseny Shishaev <ashishae@student.42.fr>
2021-08-10 20:28:59 +0200

    Rename c

--- stack.hpp
+++ stack.hpp
@@ -10,73 +10,73 @@ namespace ft
 	class stack
 	{
 	protected:
-		Container _base;
+		Container c;
 
 	public:
 		typedef T value_type;			  //	The first template parameter (T)	Type of the elements
 		typedef Container container_type; //	The second template parameter (Container)	Type of the underlying container
 		typedef size_t size_type;		  //	an unsigned integral type	usually the same as size_t
 
-		explicit stack(const container_type &ctnr = container_type()) : _base(ctnr){};
+		explicit stack(const container_type &ctnr = container_type()) : c(ctnr){};
 
 		bool empty() const
 		{
-			return _base.empty();
+			return c.empty();
 		}
 
 		value_type &top()
 		{
-			return _base.back();
+			return c.back();
 		}
 
 		const value_type &top() const
 		{
-			return _base.back();
+			return c.back();
 		}
 
 		void push(const value_type &val)
 		{
-			_base.push_back(val);
+			c.push_back(val);
 		}
 
 		void pop()
 		{
-			_base.pop_back();
+			c.pop_back();
 		}
 
 		size_type size() const
 		{
-			return _base.size();
+			return c.size();
 		}
 
 		bool operator==(const stack &rhs) const
 		{
-			return this->_base == rhs._base;
+			return this->c == rhs.c;
 		};
 
 		bool operator!=(const stack &rhs) const
 		{
-			return this->_base != rhs._base;
+			return this->c != rhs.c;
 		};
 
 		bool operator<(const stack &rhs) const
 		{
-			return this->_base < rhs._base;
+			return this->c < rhs.c;
 		};
 
 		bool operator<=(const stack &rhs) const
 		{
-			return this->_base <= rhs._base;
+			return this->c <= rhs.c;
 		};
 
 		bool operator>(const stack &rhs) const
 		{
-			return this->_base > rhs._base;
+			return this->c > rhs.c;
 		};
 
 		bool operator>=(const stack &rhs) const
 		{
-			return this->_base >= rhs._base;
+			return this->c >= rhs.c;
 		}
 	};
 
